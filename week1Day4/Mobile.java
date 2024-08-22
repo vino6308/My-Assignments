02faeefbe4a9474aa97b703942b7182c2bb2a559
vino <114132449+vino6308@users.noreply.github.com>
2024-08-22 16:04:10 +0530

    Add files via upload
    week1Day4

---
+++ week1Day4/Mobile.java
@@ -0,0 +1,26 @@
+package week1Day4;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileName = "oneplus";
+		String mobileModel = "9R";
+		int mobilePrice = 30000;
+		float mobileOffer = 4.123f;
+		double mobileSarvalue = 45.3345d;
+		char mobileLogo = '+';
+		long mobileImenumber = 6723455244l;
+		boolean ischargeravailablewiththebox = true;
+		System.out.println(mobileName);
+		System.out.println(mobileModel);
+		System.out.println(mobilePrice);
+		System.out.println(mobileOffer);
+		System.out.println(mobileSarvalue);
+		System.out.println(mobileLogo);
+		System.out.println(mobileImenumber);
+		System.out.println(ischargeravailablewiththebox);
+		System.out.println(mobileName + " " + mobileModel + "\n" + mobilePrice + "\n" + mobileOffer + "\n"
+				+ mobileSarvalue + "\n" + mobileLogo + "\n" + mobileImenumber + "\n" + ischargeravailablewiththebox);
+
+	}
+
+}
